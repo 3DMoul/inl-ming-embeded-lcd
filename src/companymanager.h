8d5c3c96490b2b270d683758321df91efa4fff1b
Svante Norrhäll <svanteman123@yahoo.se>
2026-02-28 13:24:03 +0100

    added struct for each company and gave them string names

--- src/companymanager.h
+++ src/companymanager.h
@@ -1,7 +1,22 @@
 #include <iostream>
 
 
-struct 
-{
+struct company_1{
+    std::string name = "Hederlige Harrys Bilar";
+};
+struct company_2{
+    std::string name = "Farmor Ankas Pajer AB";
+    
+};
+struct company_3{
+    std::string name = "Svarte Petters Svartbyggen";
+    
+};
+struct company_4{
+    std::string name = "Långbens detektivbyrå";
+    
+};
+struct IOT_Comurcial{
+    std::string name = "IOT:s Reklambyrå";
     
 };
