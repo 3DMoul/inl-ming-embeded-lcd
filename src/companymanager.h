97264228dd3bf0eef19c4c6294336b67c6b00a8e
Svante Norrhäll <svanteman123@yahoo.se>
2026-03-18 17:59:18 +0100

    added comments

--- src/companymanager.h
+++ src/companymanager.h
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
-//companies
-struct company_1 { char name[23]; };
-struct company_2 { char name[22]; };
-struct company_3 { char name[27]; };
-struct company_4 { char name[24]; };
-struct IOT_Commercial { char name[18]; };
+// companies
+struct company_1 { char name[23]; }; // Hederlige Harrys Bilar
+struct company_2 { char name[22]; }; // Farmor Ankas Pajer AB
+struct company_3 { char name[27]; }; // Svarte Petters Svartbyggen
+struct company_4 { char name[24]; }; // Långbens detektivbyrå
+struct IOT_Commercial { char name[18]; }; // IOT
 
-//companie ad functions
+// companie ad functions
 void company_1_advertising(struct company_1 c);
 void company_2_advertising(struct company_2 c);
 void company_3_advertising(struct company_3 c);
